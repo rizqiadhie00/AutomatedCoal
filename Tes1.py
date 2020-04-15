579fcd91b60fc0d23969e1fcec1af58d70f3ec5d
rizqiadhie00 <51319826+rizqiadhie00@users.noreply.github.com>
2020-04-16 02:45:41 +0700

    Update Tes1.py

--- Tes1.py
+++ Tes1.py
@@ -2,14 +2,20 @@ import pandas as pan
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+#Bagian ini berfungsi membaca sampel data
 df = pan.read_csv('sample_data.csv')
 df.head()
 
+#Variabel Dependent dan Variabel Independent di inisialisasi
 y = df['Gross Caloric Value']
 x = df[['Mouisture','Ash','Volatile Matter']]
 
+#Inisialisasi fungsi
 linreg = LinearRegression()
 
+#Fitting data
 linreg.fit(x,y)
 
+#Prediksi data
 ypred = linreg.predict(x)
+print(ypred)
